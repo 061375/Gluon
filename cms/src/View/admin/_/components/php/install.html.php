a6bce6a0abb9dd0c2edaab88878637108303a6bf
Jeremy Heminger <jeremy@geomail.info>
2017-01-17 16:22:05 -0800

    start work on the admin template - install

--- cms/src/View/admin/_/components/php/install.html.php
+++ cms/src/View/admin/_/components/php/install.html.php
@@ -3,6 +3,17 @@
         <?php include('head.php'); ?>
     </head>
     <body>
+        <div class="container">
+            <div class="box">
+                <img src="logo.svg" />
+                <h1>
+                    Welcome!
+                </h1>
+                to the
+                <h2>Gluon Content Management System</h2>
+                <button>Install</button>
+            </div>
+        </div>
         <?php include('footer.php'); ?>
     </body>
 </html>
